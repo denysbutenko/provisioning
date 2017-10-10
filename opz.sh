82d926c46bbdbdab3cd4600b7e27e3c7da0f729c
Joshua Li <joshua.r.li.98@gmail.com>
2017-10-10 00:10:36 -0400

    add fail2ban

--- opz.sh
+++ opz.sh
@@ -18,18 +18,22 @@ sudo apt install -y \
     silversearcher-ag \
     tmux \
     tree \
-    ufw \
     util-linux \
     vim \
     zsh
 
 sudo apt -y autoremove
 
+sudo apt install ufw
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
 sudo ufw logging on
 sudo ufw enable
+
+sudo apt install fail2ban
+
+# TODO automatic sed script to harden sshd config
 
 printf '%s\n' 'Enter new hostname:'
 read hn
