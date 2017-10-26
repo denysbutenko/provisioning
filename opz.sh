14dbe483aea73c65bc533069f070b9172bb981c1
Joshua Li <joshua.r.li.98@gmail.com>
2017-10-26 02:14:14 -0400

    opz ssh hardening

--- opz.sh
+++ opz.sh
@@ -29,7 +29,31 @@ sudo ufw enable
 
 sudo apt install -y fail2ban
 
-# TODO automatic sed script to harden sshd config
+sudo bash -c "cat > /etc/ssh/sshd_config <<EOF
+AllowUsers $(whoami)
+
+ServerKeyBits 1024
+LoginGraceTime 120
+KeyRegenerationInterval 3600
+IgnoreRhosts yes
+IgnoreUserKnownHosts yes
+StrictModes yes
+PrintMotd yes
+SyslogFacility AUTH
+LogLevel INFO
+
+PermitRootLogin no
+PermitEmptyPasswords no
+RhostsAuthentication no
+RhostsRSAAuthentication no
+X11Forwarding no
+
+RSAAuthentication yes
+PasswordAuthentication yes
+AuthenticationMethods publickey,password
+EOF"
+
+sudo service restart ssh
 
 printf '%s\n' 'Enter new hostname:'
 read hn
